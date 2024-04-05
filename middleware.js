ef997e9a9906f287a94cecc0ab6917aca07db841
Anezz12 <harsenaargrtya1@gmail.com>
2024-04-05 12:39:37 +0700

    bug

--- middleware.js
+++ middleware.js
@@ -1,27 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
 
-export const runtime = "nodejs";
-
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)", "/api(.*)"],
-  async afterAuth(auth, req) {
-    if (auth.isPublicRoute) {
-      //  For public routes, we don't need to do anything
-      return NextResponse.next();
-    }
-
-    const url = new URL(req.nextUrl.origin);
-
-    if (!auth.userId) {
-      //  If user tries to access a private route without being authenticated,
-      //  redirect them to the sign in page
-      url.pathname = "/sign-in";
-      return NextResponse.redirect(url);
-    }
-  },
+  publicRoutes: ["/api/webhook"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
