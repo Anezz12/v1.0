61c21e7e30ab608372363f9cefdf08d8420097c0
Anezz12 <harsenaargrtya1@gmail.com>
2024-04-07 01:48:32 +0700

    update

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -31,12 +31,12 @@ const LeftSideBar = () => {
   return loading || !isLoaded ? (
     <Loader />
   ) : (
-    <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
+    <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-8 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image src="/assets/logo.png" alt="logo" width={200} height={200} />
       </Link>
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-2 items-center text-light-1">
           <Link href={`/profile/${userData._id}/posts`}>
             <Image
