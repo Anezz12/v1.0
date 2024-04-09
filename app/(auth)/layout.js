4298ff2bcf87738011a33275458d48d7453551d1
Anezz12 <harsenaargrtya1@gmail.com>
2024-04-09 12:00:41 +0700

    Update

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,54 +1,35 @@
-"use client";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { useState, useEffect } from "react";
 import "../globals.css";
 
 export const layout = {
   title: "AmikomConnect",
   description: "Next 14 Social Media App",
   icons: {
-    icon: "/R.ico",
+    icon: "/favicon.ico",
   },
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
   return (
     <ClerkProvider>
-      <html lang="en" className={theme === "light" ? "light" : "dark"}>
-        <body
-          className={`${inter.className} ${
-            theme === "light" ? "bg-[#ffffff]" : "bg-[#000000]"
-          } transition-colors duration-300`}
-        >
-          <div className="min-h-screen flex flex-col justify-center items-center relative">
-            <button
-              className="absolute top-4 right-4 bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-300 px-4 py-2 rounded-md hover:bg-gray-400 dark:hover:bg-gray-600 transition-colors duration-300"
-              onClick={toggleTheme}
-            >
-              {theme === "light" ? "Dark Mode" : "Light Mode"}
-            </button>
-            <main className="max-w-4xl mx-auto px-4 py-8 bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8 transition-colors duration-300">
+      <html lang="en">
+        <body className={`${inter.className} relative`}>
+          <video
+            className="absolute top-0 left-0 w-full h-full object-cover z-0"
+            autoPlay
+            loop
+            muted
+            src="/assets/vidio.mp4"
+          />
+          <div className="min-h-screen flex flex-col justify-center items-center relative z-10">
+            <main className="max-w-4xl mx-auto px-4 py-8 bg-white bg-opacity-80 rounded-lg shadow-lg p-8">
               {children}
             </main>
           </div>
-          <footer className="text-center text-gray-800 dark:text-white py-20 transition-colors duration-300">
+          <footer className="text-center text-white py-4 relative z-10">
             <p>
               &copy; {new Date().getFullYear()} AmikomConnect. All rights
               reserved.
